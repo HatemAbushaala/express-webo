3e83d4ceefdfc47bede7a68a996a3c5297c64427
hatem abushaala <hatem.a.abushaala@gmail.com>
2025-01-23 11:31:54 +0300

    add cpu and memory examples

--- index.js
+++ index.js
@@ -10,16 +10,37 @@ var PORT = process.env.PORT || 5656
 app.listen(PORT, () => {
   console.log("Server started on port " + PORT)
   console.log("http://localhost:" + PORT)
+  console.log("node version " + process.version + "| process id:" + process.pid)
 })
 
 // app.use('/barbers', require('./routes/barberRoutes'));
 
 app.get("/", (req, res) => {
-  const envString = Object.keys(process.env).map(k=>{
-     return k + '=' + process.env[k] 
-  }).join('\n')
+  const envString = Object.keys(process.env)
+    .map((k) => {
+      return k + "=" + process.env[k]
+    })
+    .join("\n")
   res.send(`<h3>app is running in port ${PORT} </h3> \n <h4> node version ${process.version} </h4> \n <pre>app env \n ${envString}</pre>`)
 })
+
+app.get("/memory", async (req, res) => {
+  const delay = req.query.delay ?? 100
+
+  await require("./lib/memory-heavy-action")({
+    delay,
+  })
+
+  return res.send("ok")
+})
+app.get("/cpu", (req, res) => {
+  const input = req.query.input
+  const result = require("./lib/cpu-heavy-action")(input)
+
+  return res.send(`result: ${result}`)
+})
+
+
 
 app.get("*", (req, res) => {
   res.status(404).send()
